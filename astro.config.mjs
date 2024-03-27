b28545101f43fa95d5c30e211b35942183d37e2f
Roberto Fucugauchi <robbfucu@gmail.com>
2024-03-27 13:16:16 -0600

    working with linkinline

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   //site: 'https://porfolio.dev/'
-  site: 'robbfucu.github.io',
+  site: 'https://robbfucu.github.io',
 })
